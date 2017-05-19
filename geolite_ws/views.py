dc1319411f151dc397593452be76c3574a5394eb
Y.S <uvs731@ukr.net>
2017-05-19 08:05:30 +0600

    processed cases with query params

--- geolite_ws/views.py
+++ geolite_ws/views.py
@@ -15,10 +15,12 @@ db_path = '/root/data/maxmind/GeoLite2-City.mmdb'
 
 
 async def index(request):
+    """ processes request and 
+        output ip info in json format """
     
     resp_txt = ''
 
-    query = request.query
+    query = request.query 
     
     if not 'ip' in query:
         return web.Response(text='')
@@ -34,9 +36,27 @@ async def index(request):
     gp = Gip(db_path)
     
     ip_info = gp.get_info(ip)
-    
+          
     if ip_info:
-        resp_txt = json.dumps(ip_info, indent=4, sort_keys=True)
+        
+        if len(list(query.keys())) > 1: # if one need only part 
+                                        # of the ip info
+            d = dict()
+            for key in query:
+                if key == 'ip':
+                    continue
+                if not key in ip_info:
+                    continue
+                d[key] = ip_info[key]
+            
+            resp_txt = json.dumps(d, 
+                                  indent=4, 
+                                  sort_keys=True)
+            
+        else:                            
+            resp_txt = json.dumps(ip_info, 
+                                  indent=4, 
+                                  sort_keys=True)
 
 
     return web.Response(text=resp_txt)
